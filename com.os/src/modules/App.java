f8eeb219e9164f651f1c335ee2d2eb03dfaf21c0
Josumaru <jokosupriyanto1712@gmail.com>
2023-09-17 21:55:10 +0700

    editing com.os

--- com.os/src/modules/App.java
+++ com.os/src/modules/App.java
@@ -27,33 +27,47 @@ public class App {
         modules.put("1", new Modules(
                 1,
                 """
-                        \u001BSistem komputer = OS ada di komputer komputer sebagia wadahnya OS sebagai media komunikasi\u001B[0m
+                        \u001BSistem Komputer = OS ada di komputer
+                        Komputer sebagai wadahnya, OS sebagai media komunikasin\u001B[0m
                         
                         
-                        Blissmerr: Komputer adalah suatu alat electronik yang mampu melakukan bebrapa tugas 
-                        yaitu menerima input memproses dan mengeluarkan otput dalam sebuah informasi
+                        Blissmerr:  Komputer adalah suatu alat electronik yang mampu melakukan bebrapa tugas 
+                                    yaitu menerima input memproses dan mengeluarkan otput dalam sebuah informasi
                         
                         
-                        komputer adlah peralatan electromik yang dapat menerima input, mengolah in
+                        komputer adlah peralatan electronic yang dapat menerima input, mengolah input, dan mengeluarkan output dalam bentuk informasi.
                         
-                        Struktur komputer 
+                        Struktur komputer :
+                            1.  System Bus
+                            2.  ALU
+                            3.  Perangkat I/O
+                            4.  Memory:
+                                a.  RAM >> Random Access Memory >> Menyimpan data sementara
+                                b.  ROM >> Read Access Memory >> Menyimpan data secara Non Volatil
                         
-                        RAM
-                        ROM
-                        
-                        Sementara >> hanya menyimpan ketika ada tegangan listrik
-                        ROM >> <<
-                        Volatil >> Sementara
-                        Non Volatil >> <<
+                        Sementara (Volatil) >> Hanya menyimpan ketika ada tegangan listrik >> RAM
+                        Tidak Sementara (Non Volatil) >> Dapat menyimpan walaupun tidak ada sinyal listrik >> ROM
                         
                         
                         Perangkat IO
+                            Input   :   1. Keyboard
+                                        2. Mouse
+                                        3. Scanner
+                                        4. Camera
+                                        5. Joystick
+
+                            Output  :   1. Printer
+                                        2. Monitor
+                                        3. LCD Projector
+                                        4. Hologram
+                                        5. Speaker
                         
                         
-                        
-                        System Operasi
-                        1. Windows
-                        2. DLL
+                        System Operasi :
+                            1. Windows
+                            2. Linux
+                            3. Mac OS
+                            4. DLL
                                         """));
 
         for (int i = 1; i <= modules.size(); i++) {
